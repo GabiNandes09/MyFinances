8048d60503ce81b788405b8c3bf846241c17d57c
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-01-23 09:42:44 -0300

    Room totalmente configurado

--- app/src/main/java/com/rogue/financesrogue/MyApplication.kt
+++ app/src/main/java/com/rogue/financesrogue/MyApplication.kt
@@ -2,6 +2,7 @@ package com.rogue.financesrogue
 
 import android.app.Application
 import com.rogue.financesrogue.modules.appModule
+import com.rogue.financesrogue.modules.storageModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -14,7 +15,7 @@ class MyApplication: Application() {
         startKoin{
             androidLogger()
             androidContext(this@MyApplication)
-            modules(appModule)
+            modules(appModule, storageModule)
         }
     }
 }
--- app/src/main/java/com/rogue/financesrogue/modules/appModule.kt
+++ app/src/main/java/com/rogue/financesrogue/modules/appModule.kt
@@ -1,7 +1,20 @@
 package com.rogue.financesrogue.modules
 
+import androidx.room.Room
+import com.rogue.financesrogue.database.MyFinancesDatabase
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
 
+}
+
+val storageModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            MyFinancesDatabase::class.java,
+            "MyFinances.db"
+        ).build()
+    }
 }
